6a8fc07d755e4bf7fbe0b4e18e8f18a173ac03b8
Abhay Patidar <142315171+abhaypatidar3@users.noreply.github.com>
2025-05-20 22:19:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,5 @@ cloudinary.v2.config({
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.FRONTEND_URL}`);
 });
